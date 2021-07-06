753186c2798155fa390c8b7ce53f218f258ae056
DCarr40 <davidwcarruthers@yahool.com>
2021-07-06 11:25:45 -0400

    added NavBar boilerplate

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 export default function NavBar() {
     return (
-        <div>
-            
+
+        <div className = "container">
+            <nav className = "navbar navbar-expand-lg main-nav px-0">
+                <a className ="navbar-brand" href="/mojo">
+                    <img src="http://rajeshdas.com/assets/images/logo.svg" alt="rajeshdas.com"></img>
+                </a>
+
+                <button className ="navbar-toggler" type = "button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded= "false" aria-label="Toggle navigation">
+                    <span className="icon-bar icon-bar-1"></span>
+                    <span className="icon-bar icon-bar-2"></span>
+                    <span className="icon-bar icon-bar-3"></span>
+                </button>
+                <div className="collaspe navbar-collapse" id="mainMenu">
+                    <ul>
+                        <li>
+                            <a href="#home" className="active active-first">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
+
+
+    
     )
 }
